387258cfa4962c5b33542010008757731ddf6e5f
CJRivas <zeejayr@gmail.com>
2018-03-05 17:20:04 -0600

    docker login command provided by AWS is broken. known bug.

--- dockerpush.sh
+++ dockerpush.sh
@@ -2,7 +2,7 @@ cd docker
 pwd
 sudo service docker start
 sudo docker build .
-$(aws ecr get-login --region us-west-2)
+sudo $(sudo aws ecr get-login --region us-west-2| sed --expression='s/-e none//')
 sudo docker build -t kmt .
 sudo docker tag kmt:latest 142328397574.dkr.ecr.us-west-2.amazonaws.com/kmt:latest
 sudo docker push 142328397574.dkr.ecr.us-west-2.amazonaws.com/kmt:latest
